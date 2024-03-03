a1c24a937f56e99e549032c03b8402b0ab2753da
NapoleonBonaparteTheEmperor <100741449+NapoleonBonaparteTheEmperor@users.noreply.github.com>
2024-03-03 15:23:53 -0500

    Update signupin.js

--- pages/signupin.js
+++ pages/signupin.js
@@ -1,28 +1,12 @@
-import { useState } from 'react';
 import Head from 'next/head';
 import Footer from '@components/Footer';
 
 export default function Home() {
-  // State to manage the username input value
-  const [username, setUsername] = useState('');
-
-  // Function to handle form submission
-  const handleSubmit = (event) => {
+  const handleUsernameSubmit = (event) => {
     event.preventDefault();
-    
-    // Store the username in local storage
-    localStorage.setItem('username', username);
-
-    // Optional: You can redirect to another page here
-    // router.push('/some-page');
-
-    // Clear the username input field after submission
-    setUsername('');
-  };
-
-  // Function to handle username input change
-  const handleChange = (event) => {
-    setUsername(event.target.value);
+    const username = event.target.Name.value;
+    console.log("Username:", username);
+    localStorage.setItem("username", username);
   };
 
   return (
@@ -33,15 +17,19 @@ export default function Home() {
       </Head>
 
       <main>
-        <form name="SignUpName" method="POST" data-netlify="true" onSubmit={handleSubmit}>
+        <form onSubmit={handleUsernameSubmit}>
           <label htmlFor="Name">What's your username? (YOU CANNOT CHANGE THIS LATER, PLEASE MAKE IT UNIQUE.)</label><br />
-          <input type="text" id="Name" name="Name" value={username} onChange={handleChange} /><br />
+          <input type="text" id="Name" name="Name" /><br />
           <button type="submit">Submit</button>
-          <input type="hidden" name="form-name" value="SignUpName" />
+        </form>
+
+        <form name="SignUpFavCorps" method="POST" data-netlify="true">
+          <label htmlFor="FavCorps">What's your Favorite Corps?</label><br />
+          <input type="text" id="FavCorps" name="FavCorps" /><br />
         </form>
       </main>
 
       <Footer />
     </div>
-  );
+  )
 }
