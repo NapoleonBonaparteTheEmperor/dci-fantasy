7a4982effb1fdf5bee719115e8107214dbe8be77
NapoleonBonaparteTheEmperor <100741449+NapoleonBonaparteTheEmperor@users.noreply.github.com>
2024-03-02 21:39:24 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,10 +11,11 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <form>
+    <label for="Name">What's your name?
+    </label><br>
+    <input type="text" id="Name"<br>
+    </form>
       </main>
 
       <Footer />
