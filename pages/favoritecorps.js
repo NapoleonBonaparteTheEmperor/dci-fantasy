b0ce1c883565a24e96b2ae4008a9190d34e8e808
NapoleonBonaparteTheEmperor <100741449+NapoleonBonaparteTheEmperor@users.noreply.github.com>
2024-03-02 22:21:27 -0500

    Update favoritecorps.js

--- pages/favoritecorps.js
+++ pages/favoritecorps.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+
 export default function Home() {
   return (
     <div className="container">
@@ -8,9 +9,11 @@ export default function Home() {
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <main>
         <form name="SignUpFavCorps" method="POST" data-netlify="true">
           <label htmlFor="FavCorps">What's your Favorite Corps?</label><br />
           <input type="text" id="FavCorps" name="FavCorps" /><br />
+          <button type="submit">Submit</button>
         </form>
       </main>
       <Footer />
