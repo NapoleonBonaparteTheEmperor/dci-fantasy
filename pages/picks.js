4d47607338e9ee6a48dd53f9dcde3866034792d9
NapoleonBonaparteTheEmperor <100741449+NapoleonBonaparteTheEmperor@users.noreply.github.com>
2024-03-02 23:38:32 -0500

    Update picks.js

--- pages/picks.js
+++ pages/picks.js
@@ -1,1 +1,25 @@
+export default function Checklist() {
+  // Define checklist items
+  const checklistItems = [
+    "Item 1",
+    "Item 2",
+    "Item 3",
+    "Item 4",
+    "Item 5"
+  ];
+
+  return (
+    <div>
+      <h2>Checklist</h2>
+      <ul>
+        {checklistItems.map((item, index) => (
+          <li key={index}>
+            <input type="checkbox" id={`item-${index}`} />
+            <label htmlFor={`item-${index}`}>{item}</label>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
