7178c501e20e459a967dc7f2bd1889a2eb860c29
NapoleonBonaparteTheEmperor <100741449+NapoleonBonaparteTheEmperor@users.noreply.github.com>
2024-03-02 22:16:25 -0500

    Update signupin.js

--- pages/signupin.js
+++ pages/signupin.js
@@ -1,8 +1,12 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from 'next/head';
+import Link from 'next/link';
+import Footer from '@components/Footer';
 
-export default function Home() {
+export default function SignUp() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <div className="container">
       <Head>
@@ -11,14 +15,18 @@ export default function Home() {
       </Head>
 
       <main>
-        <form name="SignUpName" method="POST" data-netlify="true">
+        <form name="SignUpName" method="POST" data-netlify="true" onSubmit={handleSubmit}>
           <label htmlFor="Name">What's your username? (YOU CANNOT CHANGE THIS LATER, PLEASE MAKE IT UNIQUE.)</label><br />
           <input type="text" id="Name" name="Name" /><br />
+          <button type="submit">Submit</button>
+          <input type="hidden" name="form-name" value="SignUpName" />
         </form>
-
+        
+        <Link href="/">Home</Link> <br />
+        <Link href="/favoritecorps">New Page</Link> <br />
       </main>
 
       <Footer />
     </div>
-  )
+  );
 }
